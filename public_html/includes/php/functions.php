0ab06d500fdb924526ccbdeabadc0866e1bdfc59
jdorado <javier@offlinela.com>
2013-06-27 16:00:37 -0700

    move json pretty print to json_functions

--- public_html/includes/php/functions.php
+++ public_html/includes/php/functions.php
@@ -250,49 +250,4 @@ function email($from, $fromEmail, $to, $toEmail, $subject, $htmlBody, $plainBody
 }
 
 
-function json_pretty($json, $indent='  ')
-{
-    is_array($json ) || is_object($json )? $json = json_encode($json) : null;
-    $result      = '';
-    $pos         = 0;
-    $strLen      = strlen($json);
-    $indentStr   = $indent;
-    $newLine     = "\n";
-    $prevChar    = '';
-    $outOfQuotes = true;
-
-    for ($i=0; $i<=$strLen; $i++) {
-
-        $char = substr($json, $i, 1);
-
-        if ($char == '"' && $prevChar != '\\') {
-            $outOfQuotes = !$outOfQuotes;
-        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
-            $result .= $newLine;
-            $pos --;
-            for ($j=0; $j<$pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-
-        $result .= $char;
-
-        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
-            $result .= $newLine;
-            if ($char == '{' || $char == '[') {
-                $pos ++;
-            }
-
-            for ($j = 0; $j < $pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-
-        $prevChar = $char;
-    }
-
-    return $result;
-}
-
-
 ?>
