eebdfe351b6d20014b257a95542ea865f9766c8d
jdorado <javier@offlinela.com>
2013-07-03 15:05:40 -0700

    delete _old folders..

--- public_html/_OLD/imagefeed/removePost.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once "db.php";
-
-$queryStatment = "UPDATE imageInfo SET posted=1 WHERE imageURL = '".$_REQUEST['imageName']."' OR bigImageURL = '".$_REQUEST['imageName']."'";
-mysql_query($queryStatment);
-mysql_close($con);
-?>
--- public_html/_OLD/includes/function.php
+++
@@ -1,6 +0,0 @@
-<?php
-function resultArray($success, $result) {
-	$ret = array('success' => ($success == TRUE ? TRUE : FALSE), 'data' => $result);
-	return $ret;
-}
-?>
