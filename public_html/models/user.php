df7355945cdcd8ac126a3dcef8b955f03cb90e06
Javier Dorado - github <javier@offlinela.com>
2013-11-05 17:18:54 -0800

    fix membership level query

--- public_html/models/user.php
+++ public_html/models/user.php
@@ -203,8 +203,8 @@ class User extends db {
                   ml.name
                   FROM membership_level ml, user_username user
                   WHERE user.user_id = user_username.user_id
-                  AND ABS(CAST(ml.points AS SIGNED) - CAST(user.points AS SIGNED) ) / ml.points > 1
-                  order by ABS(CAST(ml.points AS SIGNED) - CAST(user.points AS SIGNED) +1) ASC
+                  	AND CAST(user.points AS SIGNED)  / ml.points > 1
+                  order by ABS(CAST(ml.points AS SIGNED) - CAST(user.points AS SIGNED)) ASC
                   LIMIT 1
               ) AS membership_level
               ,(
@@ -535,7 +535,7 @@ class User extends db {
       			' . $this->generate_limit_offset_str($params) . '
       		';
 
-    */
+        */
 
         $following_query = "
             SELECT distinct
